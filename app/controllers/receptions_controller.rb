f63811e3dfab9bbab84bee31d01143d655546634
yasu0408 <choppaya.ketsukacchin@gmail.com>
2020-11-17 15:15:58 +0900

    新規投稿機能を実装

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -1,14 +1,19 @@
 class ReceptionsController < ApplicationController
   def index
+    user = User.new
+    @my_introduce = user.introduce
   end
 
   def show
   end
 
   def new
+    @reception = Reception.new
   end
 
   def create
+    reception = Reception.create!(reception_params)
+    redirect_to reception
   end
 
   def edit
@@ -18,5 +23,11 @@ class ReceptionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def reception_params
+    params.require(:post).permit(:name, :arrive, :leave)
   end
 end
