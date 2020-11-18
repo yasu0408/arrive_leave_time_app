734d73190160a265b97f2c13a3fd3e955a135dbb
yasu0408 <choppaya.ketsukacchin@gmail.com>
2020-11-18 13:21:10 +0900

    Receptionモデルにバリデーションを追加

--- app/models/reception.rb
+++ app/models/reception.rb
@@ -1,2 +1,11 @@
 class Reception < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :arrive, presence: true
+  validates :leave, presence: true
+  validate :arrive_leave_check
+end
+
+def arrive_leave_check
+  errors.add(:leave, "をきちんとなおしましょう") unless
+    arrive < leave
 end
